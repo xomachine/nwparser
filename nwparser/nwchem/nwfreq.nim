0602f68b85ce5baf9bccee339e17143b5b2e0e08
xomachine <xomachiner@gmail.com>
2017-05-19 16:46:15 +0300

    And a bit more code, not in mood even to complete the procedure

--- nwparser/nwchem/nwfreq.nim
+++ nwparser/nwchem/nwfreq.nim
@@ -1,12 +1,22 @@
-from utils import skipLines
-from structures import Calculation, CalcType
+from utils import skipLines, floatPattern
+from structures import Calculation, CalcType, Mode, ThermoData
 from streams import Stream, atEnd, readLine
 
 const FreqHeader = """\s*'NWChem Nuclear Hessian and Frequency Analysis'.*"""
 let rotationalPattern = peg"'Rotational constants'"
 let freqPattern = peg"\s*'NORMAL MODE EIGENVECTORS IN CARTESIAN COORDINATES'"
 
-proc readFreq(fd: Stream): Calculation =
+proc readThermal(fd: Stream): ThermoData =
+  discard
+proc readModes(fd: Stream): seq[Mode] =
+  var captures = newSeq[string](6)
+  let elemPattern: static[Peg] = peg("""$1(\s+{$1})?(\s+{$1})?(\s+{$1})?(\s+{$1})?(\s+{$1})?""" % floatPattern)
+  while not fd.atEnd():
+    let line = fd.readLine()
+    while fd.readLine().match(elemPattern, captures):
+      discard
+
+proc readFreq*(fd: Stream): Calculation =
   result.kind = CalcType.Frequency
   while not fd.atEnd():
     let line = fd.readLine()
