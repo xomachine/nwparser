8918905f8496d622884f6e60f0105f072621da9c
xomachine <xomachiner@gmail.com>
2017-06-23 18:22:03 +0300

    Driver and stepper optimizer output recognition

--- nwparser/nwchem/nwopt.nim
+++ nwparser/nwchem/nwopt.nim
@@ -7,36 +7,67 @@ from nwgeometry import findGeometry
 
 const OptHeader* = """^\s*'NWChem Geometry Optimization'$"""
 let multiplicityPattern* = peg"^\s*('Spin'\s+)?[mM]'ult'\D+{\d+}\D*$"
+let endPattern = peg"^\s*'Task'\s+'times'\s+'cpu:'.*$"
+proc readStepper(fd: Stream): Calculation
+proc readDriver(fd: Stream): Calculation
 
 proc readOpt*(fd: Stream): Calculation =
+  let driverPattern {.global.} = peg"^\s*'Energy Minimization'\s*$"
+  let stepperPattern {.global.} = peg"^\s*'NWChem STEPPER Module'\s*$"
+  while not fd.atEnd():
+    let line = fd.readLine()
+    if line.match(driverPattern):
+      result = fd.readDriver()
+    elif line.match(stepperPattern):
+      result = fd.readStepper()
+    else: continue
+    break
+
+proc readStepper(fd: Stream): Calculation =
+  stderr.writeLine("Reading stepper optimization calculation...")
+  let stepStartPattern {.global.} = peg"^\s*'Walk statistics for step:'\s*{\d+}\s*$"
+  let energyPattern {.global.} = peg("""^\s*'Current actual total energy:'\s*{""" &
+                                     floatPattern & """D[+-]\d+}\s*$""")
+  result.multiplicity = fd.find(multiplicityPattern)[0].parseInt()
+  result.kind = CalcType.Optimization
+  result.path = newSeq[PESPoint]()
+  while not fd.atEnd():
+    let patternIndex = fd.findAny(stepStartPattern, endPattern)
+    if patternIndex == 1:
+      break
+    let energyCapture = fd.find(energyPattern)
+    let geometry = fd.findGeometry()
+    let energy = energyCapture[0].parseFloat().Hartree()
+    let point = (geometry: geometry, energy: energy)
+    result.path.add(point)
+
+proc readDriver(fd: Stream): Calculation =
+  stderr.writeLine("Reading driver optimization calculation...")
+  result.kind = CalcType.Optimization
   let energyPattern {.global.} =
     peg("""^'@'\s+{\d+}\s+{""" & floatPattern & """}.*$""")
   let stepPattern {.global.} = peg"^\s*'Step'\s+{\d+}.*$"
   let convergedPattern {.global.} = peg"^\s*'Optimization converged'\s*$"
-  let endPattern {.global.} = peg"^\s*'Task'\s+'times'\s+'cpu:'.*$"
-  result.kind = CalcType.Optimization
   result.path = newSeq[PESPoint]()
-  stderr.writeLine("Reading optimization calculation...")
   while not fd.atEnd():
     let patternIndex = fd.findAny(stepPattern, convergedPattern, endPattern)
     case patternIndex
+    of 0:
+      let stepGeometry = fd.findGeometry(nobonds = true)
+      if result.multiplicity == 0:
+        let multiplicityCaptures = fd.find(multiplicityPattern,
+                                           "Can not find multiplicity")
+        result.multiplicity = multiplicityCaptures[0].parseInt()
+        stderr.writeLine("Multiplicity = " & $result.multiplicity)
+      let energyCaptures = fd.find(energyPattern, "Can not find energy")
+      let energy = energyCaptures[1].parseFloat().Hartree()
+      stderr.writeLine("Energy = " & $energy)
+      let point = (geometry: stepGeometry, energy: energy)
+      result.path.add(point)
+      continue
     of 1:
       let energyCaptures = fd.find(energyPattern, "Can not find energy")
       result.path[^1].geometry = fd.findGeometry()
       result.path[^1].energy = energyCaptures[1].parseFloat().Hartree()
-      break
-    of 2:
-      stderr.writeLine("Found end...")
-      break
     else: discard
-    let stepGeometry = fd.findGeometry(nobonds = true)
-    if result.multiplicity == 0:
-      let multiplicityCaptures = fd.find(multiplicityPattern,
-                                         "Can not find multiplicity")
-      result.multiplicity = multiplicityCaptures[0].parseInt()
-      stderr.writeLine("Multiplicity = " & $result.multiplicity)
-    let energyCaptures = fd.find(energyPattern, "Can not find energy")
-    let energy = energyCaptures[1].parseFloat().Hartree()
-    stderr.writeLine("Energy = " & $energy)
-    let point = (geometry: stepGeometry, energy: energy)
-    result.path.add(point)
+    break
